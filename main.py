715368a5e413ead8a9e1abc031df912f2b409609
Jiahonzheng <jiahonzheng@gmail.com>
2019-09-27 20:16:02 +0800

    feat: 完成货物自动搬运脚本
    使用 OpenCV 模板匹配，实现货物自动搬运脚本。

---
+++ main.py
@@ -0,0 +1,19 @@
+from automator import Automator
+from target import TargetType
+
+if __name__ == '__main__':
+    # 声明货物要移动到的建筑 ID 。
+    targets = {
+        TargetType.Chair: 1,
+        TargetType.Wood: 7,
+        TargetType.Bottle: 4,
+        TargetType.Vegetable: 5,
+        TargetType.Box: 2,
+        TargetType.Food: 8
+    }
+
+    # 连接 adb 。
+    instance = Automator('127.0.0.1:7555', targets)
+
+    # 启动脚本。
+    instance.start()
