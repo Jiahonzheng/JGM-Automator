894c17b78b76bb84895db6be4f208f4a70de5247
TsingWei <461626239@qq.com>
2019-10-08 23:20:07 +0800

    默认为MuMu启动, 自动连接MuMu

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from automator import Automator
+import subprocess
 
 if __name__ == '__main__':
     # 升级建筑列表, 实际升级是随机从这个列表中挑一个升级
-    #up_list = [(1,5),(2,5),(3,5),(4,5),(5,5),(6,5),(7,5),(8,5),(9,5)]  # 雨露均沾
+    # up_list = [(1,5),(2,5),(3,5),(4,5),(5,5),(6,5),(7,5),(8,5),(9,5)]  # 雨露均沾
     # up_list = [(1,1),(1,1),(1,1),(4,3)] # 75%的概率1号升级1次， 25%的概率4号升级3次
     up_list = [(8,1),(9,1)] # 这个号建筑升级1次， 那个号建筑升级1次
     # 收货过滤列表
@@ -17,7 +18,9 @@ if __name__ == '__main__':
     policy = True # 是否自动升级政策
     task = True # 是否自动完成任务
     speed_up = True # 是否自动重启加速刷火车
-    instance = Automator(Device2, up_list, harvest_filter,auto_policy=policy,auto_task=task,speedup=speed_up)
+    if b'connected' in subprocess.check_output('adb connect '+ MuMu):
+        print("Successfully connected to MuMu.")
+    instance = Automator(MuMu, up_list, harvest_filter,auto_policy=policy,auto_task=task,speedup=speed_up)
     # 启动脚本。
     instance.start()
     
